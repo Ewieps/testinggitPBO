a896bfbf9452b60d6a7eadb52f30c4413cf1e151
Ewieps <comradesayosa@gmail.com>
2025-03-03 00:33:57 +0700

    testcommt

---
+++ tutorPBO/testinggpbo/testinggpbo/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace testinggpbo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
+
